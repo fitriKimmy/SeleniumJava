358e807c420824b4fad05e81032a6e0c8a5501b8
fitri.manurung <fitri.manurung@go-jek.com>
2020-09-05 18:39:21 +0700

    Add keyboard mouse input test

---
+++ src/test/java/KeyboardAndMouseInput.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class KeyboardAndMouseInput {
+    public static void main(String[] args) {
+        System.getProperty("webdriver.chrome.driver", "/Users/fitri.manurung/Workspace/Tools/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/keypress");
+
+        WebElement name = driver.findElement(By.id("name"));
+        name.click();
+        name.sendKeys("fitriKimmy");
+
+        WebElement button = driver.findElement(By.id("button"));
+        button.click();
+
+        driver.quit();
+    }
+}
