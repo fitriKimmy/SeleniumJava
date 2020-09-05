8caad60c124a5efc2c4ad4acc5305a5e13188051
fitri.manurung <fitri.manurung@go-jek.com>
2020-09-05 22:44:03 +0700

    Cleaning code Form test

--- src/test/java/Form.java
+++ src/test/java/Form.java
@@ -10,26 +10,34 @@ import static junit.framework.TestCase.assertEquals;
 public class Form {
     public static void main(String[] args) {
         System.getProperty("webdriver.chrome.driver", "/Users/fitri.manurung/Workspace/Tools/chromedriver");
-
         WebDriver driver = new ChromeDriver();
         driver.get("https://formy-project.herokuapp.com/form");
 
-       driver.findElement(By.id("first-name")).sendKeys("Alex");
-       driver.findElement(By.id("last-name")).sendKeys("Sandra");
-       driver.findElement(By.id("job-title")).sendKeys("Test Engineer");
-       driver.findElement(By.id("radio-button-2")).click();
-       driver.findElement(By.id("checkbox-2")).click();
-       driver.findElement(By.cssSelector("option[value='1']")).click();
-       driver.findElement(By.id("datepicker")).sendKeys("10/10/2022");
-       driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);;
-       driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+        submitForm(driver);
+        waitForAlertBanner(driver);
+        assertEquals("The form was successfully submitted!", getBannerText(driver));
 
-       WebDriverWait wait = new WebDriverWait(driver, 10);
-       WebElement alert = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
+        driver.quit();
+    }
 
-       String alertText = alert.getText();
-       assertEquals("The form was successfully submitted!", alertText);
+    public static void submitForm(WebDriver driver){
+        driver.findElement(By.id("first-name")).sendKeys("Alex");
+        driver.findElement(By.id("last-name")).sendKeys("Sandra");
+        driver.findElement(By.id("job-title")).sendKeys("Test Engineer");
+        driver.findElement(By.id("radio-button-2")).click();
+        driver.findElement(By.id("checkbox-2")).click();
+        driver.findElement(By.cssSelector("option[value='1']")).click();
+        driver.findElement(By.id("datepicker")).sendKeys("10/10/2022");
+        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);;
+        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+    }
 
-       driver.quit();
+    public static void waitForAlertBanner(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement alert = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert")));
+    }
+
+    public static String getBannerText(WebDriver driver){
+        return driver.findElement(By.className("alert")).getText();
     }
 }
