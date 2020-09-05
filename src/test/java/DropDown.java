e3559818f2b85f599b13d481a4e241052736dd22
fitri.manurung <fitri.manurung@go-jek.com>
2020-09-05 21:50:22 +0700

    Add dare picker and drop down test

---
+++ src/test/java/DropDown.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DropDown {
+    public static void main(String[] args) {
+        System.getProperty("webdriver.chrome.driver", "/Users/fitri.manurung/Workspace/Tools/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/dropdown");
+
+        WebElement dropDownMenu = driver.findElement(By.id("dropdownMenuButton"));
+        dropDownMenu.click();
+
+        WebElement autoCompleteItem = driver.findElement(By.id("autocomplete"));
+        autoCompleteItem.click();
+
+        driver.quit();
+    }
+}
